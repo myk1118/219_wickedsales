c36515fff7326df69291dcea7e5058c3b3bae39b
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-04-04 15:15:15 -0700

    Completed mobile responsive nav and side nav

--- src/components/nav/sidenav.js
+++ src/components/nav/sidenav.js
@@ -3,21 +3,12 @@ import { Link } from 'react-router-dom';
 
 class Sidenav extends Component {
     componentDidMount() {
-        console.log('Sidenav Mounted:', this.sidenav);
         M.Sidenav.init(this.sidenav);
     }
     render() {
         return (
             <ul ref={(element) => { this.sidenav = element }} id="sidenav" className="sidenav">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-                <li>
-                    <Link to="/">Sign In</Link>
-                </li>
+                {this.props.links}
             </ul>
         );
     }
