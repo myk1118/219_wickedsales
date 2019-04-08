5ddf441f07bf468f52bba2b63c11a0f17fb147b0
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-04-08 16:10:06 -0700

    Completed product details page

--- src/components/products/product_carousel.js
+++ src/components/products/product_carousel.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 class ProductCarousel extends Component {
     componentDidMount() {
-        console.log('Carousel Div:', this.carousel);
-        M.Carousel.init(this.carousel);
+        const config = {
+            numVisible: 1,
+            indicators: true,
+            fullWidth: true
+        };
+
+        M.Carousel.init(this.carousel, config);
     }
 
     render() {
-        console.log('Props:', this.props);
-
         const items = this.props.images.map((img) => {
             return (
                 <a key={img} className="carousel-item" href="#">
@@ -18,7 +21,7 @@ class ProductCarousel extends Component {
         });
 
         return (
-            <div ref={(element) => this.carousel = element} className="carousel">
+            <div ref={(element) => this.carousel = element} className="col s12 m8 carousel carousel-slider">
                 {items}
             </div>
         );
--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ProductCarousel from './product_carousel';
+import MiscDetails from './misc_details';
+import { formatMoney } from '../../helpers';
 
 class ProductDetails extends Component {
     state = {
@@ -19,8 +21,6 @@ class ProductDetails extends Component {
 
         const resp = await axios.get(`/api/getproductdetails.php?productId=${params.product_id}`);
 
-        console.log('Resp:', resp);
-
         if (resp.data.success) {
             this.setState({
                 details: resp.data.productInfo
@@ -33,7 +33,6 @@ class ProductDetails extends Component {
     }
 
     render() {
-        console.log('Product Details:', this.state.details);
         const { details } = this.state;
 
         if (details === null) {
@@ -42,7 +41,7 @@ class ProductDetails extends Component {
             return <h1 className="center">No Product Found</h1>
         }
 
-        const { description = 'No description available', name, images, price } = details;
+        const { description = 'No description available', name, images, price, miscDetails } = details;
         // const imageList = images.map((image, index) => {
         //     return (
         //         <img key={index} src={`/dist/${image}`} />
@@ -51,9 +50,28 @@ class ProductDetails extends Component {
         return (
             <div className="product-details" >
                 <h1 className="center">{name}</h1>
-                <ProductCarousel images={images} />
-                <p> ${(price / 100).toFixed(2)}</p >
-                <p>{description}</p>
+                <div className="row">
+                    <ProductCarousel images={images} />
+                    <div className="col s12 m8">
+                        <div className="right-align product-price">{formatMoney(price)}</div>
+                        <div className="right-align add-to-cart">
+                            <span className="qty-container">
+                                <button className="btn btn-small btn-floating green lighten-1">
+                                    <i className="material-icons">remove</i>
+                                </button>
+                                <span className="product-qty">1</span>
+                                <button className="btn btn-small btn-floating green lighten-1">
+                                    <i className="material-icons">add</i>
+                                </button>
+                            </span>
+                            <button className="btn green darken-2">
+                                <i className="material-icons">add_shopping_cart</i>
+                            </button>
+                        </div>
+                        <p>{description}</p>
+                        <MiscDetails details={miscDetails} />
+                    </div>
+                </div>
             </div >
         );
     }
