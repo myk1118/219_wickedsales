764da6f869ec5691051a91491d55b265253ccaf3
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-04-08 18:04:53 -0700

    Added the ability to increase the quantity inside cart items

--- public/api/addcartitem.php
+++ public/api/addcartitem.php
@@ -8,7 +8,7 @@ require_once('config.php');
 
 require_once('mysqlconnect.php'); //makes a connection to the database
 
-if(empty($_GET['product_id'])) {
+if (empty($_GET['product_id'])) {
     throw new Exception('You must send a product_id (int) with your request');
 }
 
@@ -20,11 +20,11 @@ $query = "SELECT `price` FROM `products` WHERE id = $product_id";
 
 $result = mysqli_query($conn, $query);
 
-if(!$result) {
+if (!$result) {
     throw new Exception(mysqli_error($conn));
 }
 
-if(mysqli_num_rows($result) === 0) {
+if (mysqli_num_rows($result) === 0) {
     throw new Exception("no product matches product id $product_id");
 }
 
@@ -34,7 +34,7 @@ $product_price = (int)$product_data['price']; //makes price into an integer; sam
 
 $product_total = $product_price * $product_quantity;
 
-if(empty($_SESSION['cart_id'])) { // if there is no cart in the session we will make one
+if (empty($_SESSION['cart_id'])) { // if there is no cart in the session we will make one
     $cart_create_query = "INSERT INTO `carts` SET
         `item_count` = $product_quantity,
         `total_price` = $product_total,
@@ -43,16 +43,31 @@ if(empty($_SESSION['cart_id'])) { // if there is no cart in the session we will 
         `changed` = NOW()
     ";
     $cart_result = mysqli_query($conn, $cart_create_query);
-    if(!$cart_result) {
+    if (!$cart_result) {
         throw new Exception(mysqli_error($conn));
     }
-    if(mysqli_affected_rows($conn) === 0) {
+    if (mysqli_affected_rows($conn) === 0) {
         throw new Exception('data was not added to cart table');
     }
     $cart_id = mysqli_insert_id($conn);
     $_SESSION['cart_id'] = $cart_id;
 } else {
     $cart_id = $_SESSION['cart_id']; //if we already have a cart in the session we will use that one
+
+    $update_cart_query = "UPDATE `carts` SET
+        `item_count` = `item_count` + $product_quantity,
+        `total_price` = `total_price` + $product_total
+        WHERE `id` = $cart_id";
+
+    $update_result = mysqli_query($conn, $update_cart_query);
+
+    if (!$update_result) {
+        throw new Exception(mysqli_error($conn));
+    }
+
+    if (mysqli_affected_rows($conn) === 0) {
+        throw new Exception('Cart data was not updated');
+    }
 }
 
 $cart_item_query = "INSERT INTO `cart_items` SET
@@ -65,11 +80,11 @@ $cart_item_query = "INSERT INTO `cart_items` SET
 
 $cart_item_result = mysqli_query($conn, $cart_item_query);
 
-if(!$cart_item_result) {
+if (!$cart_item_result) {
     throw new Exception(mysqli_error($conn));
 }
 
-if(mysqli_affected_rows($conn) === 0) {
+if (mysqli_affected_rows($conn) === 0) {
     throw new Exception('failed to insert into cart items');
 }
 
@@ -80,5 +95,3 @@ $output = [
 ];
 
 print(json_encode($output));
-
-?>
--- public/api/sessiontest.php
+++ public/api/sessiontest.php
@@ -2,6 +2,6 @@
 
 session_start();
 
+echo '<pre>';
 print_r($_SESSION);
-
-?>
+echo '</pre>';
