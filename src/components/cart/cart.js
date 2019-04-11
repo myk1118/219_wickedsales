0d501bf9ed779435802aed9ce3a327ff6f2e0aaa
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-04-10 18:24:00 -0700

    Gave the cart the ability to display items

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,20 +1,82 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { formatMoney } from '../../helpers';
+import './cart.scss';
 
 class Cart extends Component {
-    getCartData = async () => {
-        // const resp = axios.get('/api/');
+    state = {
+        items: [],
+        meta: {}
+    }
+
+    componentDidMount() {
+        this.getCartData();
+    }
+
+    async getCartData() { //you don't need to bind this function because...?
+        const { data = {} } = await axios.get('/api/getcartitems.php');
+
+        if (data.success) {
+            this.setState({
+                items: data.cartItems,
+                meta: data.cartMetaData
+            });
+        } else {
+            console.log('Cart data failed to load');
+        }
     }
 
     render() {
+        console.log('State:', this.state);
+        const { items, meta } = this.state;
+        let totalItems = 0;
+
+        const cartItems = items.map(({ name, price, image, quantity, id }) => {
+            totalItems += quantity;
+            const itemTotalPrice = formatMoney(quantity * price);
+            return (
+                <tr key={id}>
+                    <td>
+                        <img src={`/dist/${image}`} alt={`${name} product image`} />
+                    </td>
+                    <td>{name}</td>
+                    <td>{formatMoney(price)}</td>
+                    <td>{quantity}</td>
+                    <td>{itemTotalPrice}</td>
+                </tr>
+            )
+        });
+
         return (
-            <div>
+            <div className="cart" >
                 <h1 className="center">Shopping Cart</h1>
+
                 <Link to="/products">Continue Shopping</Link>
+
+                <div className="right-align total-items">Total Items in Cart: {totalItems}</div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Product Name</th>
+                            <th>Price Each</th>
+                            <th>Quantity</th>
+                            <th>Item Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cartItems}
+                        <tr>
+                            <td colSpan="5" className="total-price">
+                                Total: {formatMoney(meta.total)}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
-}
+};
 
 export default Cart;
