0e7ee396bd16246c7397a690477b717321ba69de
Mi Kim <jaime1118@Jaimes-MacBook-Pro.local>
2019-04-12 13:36:37 -0700

    Started building sign in form

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import '../assets/css/app.scss';
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
+import AccountRoutes from './account';
 import Cart from './cart';
 import Home from './home';
 import Nav from './nav';
@@ -48,6 +49,7 @@ class App extends Component {
                             return <ProductRoutes {...routingProps} updateCart={this.updateCartItems} />
                         }} />
                         <Route path="/cart" component={Cart} />
+                        <Route path="/account" component={AccountRoutes} />
                         <Route component={NotFound} />
                     </Switch>
                 </div>
---
+++ src/reducers/index.js
@@ -0,0 +1,35 @@
+// const state = {
+//     user: {
+//         auth: false,
+//         username: 'Steve'
+//     },
+//     products: {
+//         list: [],
+//         productDetails: {}
+//     },
+//     cart: {
+//         totalItems: 4,
+//         totalCost: 8900,
+//         items: []
+//     }
+// }
+
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import userReducer from './user_reducer';
+
+const rootReducer = combineReducers({
+    // map of what the state is going to look like, you don't have to write out state variable, combineReducers is doing it for you
+    form: formReducer,
+    user: userReducer // calling userReducer function
+});
+
+// const state = {
+//     form: {},
+//     user: {
+//         auth: false,
+//         username: ''
+//     }
+// }
+
+export default rootReducer;
